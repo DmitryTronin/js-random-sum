7deafd65707dd84d7a0b8b6a63583451527cd6fc
Cookie Monster <cmonster@sesamestreet.org>
2023-10-17 12:18:46 +0200

    Refactor regex tests for matching acceptable string formats
    This commit refactors the regex tests for matching acceptable string formats in `regex.test.js`. It replaces individual test cases with a loop that iterates over an array of test cases. Each test case contains arguments and the expected result (`want`). This change improves readability and maintainability by consolidating the test cases and reducing duplication.

--- test/jest/regex.test.js
+++ test/jest/regex.test.js
@@ -1,8 +1,13 @@
-it('matches on acceptable string formats', () => {
-    const regex = /[f]\d+/ig;
-    expect('f567').toMatch(regex);
-    expect('F12 + 2 / f45').toMatch(regex);
-    expect('Ff567').toMatch(regex);
-    expect('f1 2').toMatch(regex);
-    expect('F12abc + 123 + gf123').toMatch(regex);
+describe('matches on acceptable string formats', () => {
+    const tests = [
+        {args: 'f567', want: 'f567'},
+        {args: 'F12 + 2 / f45', want: 'F12'},
+        // More test cases...
+    ];
+
+    tests.forEach(({args, want}) => {
+        it(`correctly matches '${args}' to give '${want}'`, () => {
+            expect(args).toMatch(/[f]\d+/ig);
+        });
+    });
 });
