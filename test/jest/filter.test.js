8ce673358db055992a4c3f1d17661e2372ba0868
Cookie Monster <cmonster@sesamestreet.org>
2023-12-11 16:22:19 +0100

    feat: Add configuration for node.js and npm scripts
    This commit adds a new `.fleet/run.json` file with configurations for running a Node.js application and npm scripts. The configuration includes a Node.js configuration with the entry point set to `src/app.js` and an npm configuration with the command `run` and the `open-my-html` script.

--- test/jest/filter.test.js
+++ test/jest/filter.test.js
@@ -1,34 +1,34 @@
 describe("Filter function", () => {
     const input = [
-        { id: 1, url: "https://www.url1.dev" },
-        { id: 2, url: "https://www.url2.dev" },
-        { id: 3, url: "https://www.link3.dev" }
+        {id: 1, url: "https://www.url1.dev"},
+        {id: 2, url: "https://www.url2.dev"},
+        {id: 3, url: "https://www.link3.dev"}
     ];
-    
+
     test("it should filter by a search term (link)", () => {
-        const output = [{ id: 3, url: "https://www.link3.dev" }];
+        const output = [{id: 3, url: "https://www.link3.dev"}];
 
         expect(filterByTerm(input, "link")).toEqual(output);
-        
+
         expect(filterByTerm(input, "LINK")).toEqual(output);
     });
-    
+
     test("it should filter by a search term (url)", () => {
 
-        const output = [{ id: 1, url: "https://www.url1.dev" }, { id: 2, url: "https://www.url2.dev" }];
+        const output = [{id: 1, url: "https://www.url1.dev"}, {id: 2, url: "https://www.url2.dev"}];
 
         expect(filterByTerm(input, "uRL")).toEqual(output);
     });
-    
+
     test("it should filter by an empty search term", () => {
         const output = [
-            { id: 1, url: "https://www.url1.dev" },
-            { id: 2, url: "https://www.url2.dev" },
-            { id: 3, url: "https://www.link3.dev" }
+            {id: 1, url: "https://www.url1.dev"},
+            {id: 2, url: "https://www.url2.dev"},
+            {id: 3, url: "https://www.link3.dev"}
         ];
         expect(filterByTerm(input, "")).toEqual(output);
     });
-    
+
     test("it should not filter by an invalid search term", () => {
         const output = [
         ];
@@ -38,7 +38,7 @@ describe("Filter function", () => {
 
 function filterByTerm(inputArr, searchTerm) {
     const regex = new RegExp(searchTerm, "i");
-    return inputArr.filter(function(arrayElement) {
+    return inputArr.filter(function (arrayElement) {
         return arrayElement.url.match(regex);
     });
 }
--- test/jest/sum.test.js
+++ test/jest/sum.test.js
@@ -1,13 +1,35 @@
-const { sum, multiply, subtract } = require('./sum.js');
+describe('Testing sum, multiply and subtract functions', () => {
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
+    const { sum, multiply, subtract } = require('./sum.js');
 
-test('multiplies 1 and 2 to equal 2', () => {
-    expect(multiply(1, 2)).toBe(2);
-});
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
 
-test('subtracts 1 and 2 to equal -1', () => {
-    expect(subtract(1, 2)).toBe(-1);
+
+    /**
+     * Here we are testing the functions of the sum.js file. 
+     * The functions include "sum", "multiply" and "subtract".
+     * Each one is tested with a simple use case. 
+     * 
+     * For a detailed understanding, we encourage you to refer the 
+     * documentation at : {@link https://example.com/yourDocLinkHere}
+     */
+    test('multiplies 1 and 2 to equal 2', () => {
+        expect(multiply(1, 2)).toBe(2);
+    });
+
+    test('subtracts 1 and 2 to equal -1', () => {
+        expect(subtract(1, 2)).toBe(-1);
+    });
+
+
+    test('this test will always fail', () => {
+        expect(true).toBe(false);
+    });
+
+//    test.skip('this test will be ignored', () => {
+//        expect(true).toBe(true);
+//    });
+
 });
