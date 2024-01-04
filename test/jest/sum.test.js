21dd710c7962d53cda92dc497298561ab4bb24cd
Cookie Monster <cmonster@sesamestreet.org>
2024-01-04 14:44:05 +0100

    refactorings

--- test/jest/sum.test.js
+++ test/jest/sum.test.js
@@ -1,20 +1,11 @@
 describe('Testing sum, multiply and subtract functions', () => {
 
-    const { sum, multiply, subtract } = require('./sum.js');
+    const {sum, multiply, subtract} = require('../../src/sum.js');
 
     test('adds 1 + 2 to equal 3', () => {
         expect(sum(1, 2)).toBe(3);
     });
 
-
-    /**
-     * Here we are testing the functions of the sum.js file. 
-     * The functions include "sum", "multiply" and "subtract".
-     * Each one is tested with a simple use case. 
-     * 
-     * For a detailed understanding, we encourage you to refer the 
-     * documentation at : {@link https://example.com/yourDocLinkHere}
-     */
     test('multiplies 1 and 2 to equal 2', () => {
         expect(multiply(1, 2)).toBe(2);
     });
@@ -28,8 +19,8 @@ describe('Testing sum, multiply and subtract functions', () => {
         expect(true).toBe(false);
     });
 
-//    test.skip('this test will be ignored', () => {
-//        expect(true).toBe(true);
-//    });
+    test.skip('this test will be ignored', () => {
+        expect(true).toBe(true);
+    });
 
 });
