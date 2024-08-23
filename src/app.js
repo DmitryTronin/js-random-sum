7e93870088ce5ae958f78d4df329e109aabe9af7
Static Newman <dmitry.tronin@jetbrains.com>
2024-08-23 14:56:27 +0200

    1

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-window.onload = function() {
+window.addEventListener('load', function() {
     var num1 = Math.floor(Math.random() * 100) + 1; // generates a random number from 1 to 100 inclusive
     var num2 = Math.floor(Math.random() * 100) + 1; // generates a random number from 1 to 100 inclusive
     var sum = num1 + num2;
     var elem = document.getElementById('sum');
     
-    if(elem){
+    if (elem !== null) {
         elem.innerText = "The sum of " + num1 + " and " + num2 + " is " + sum;
     } else {
         console.log("Element with ID 'sum' not found");
     }
-};
+});
